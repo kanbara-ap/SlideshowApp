93c0668139e35eeaac22127912ee25841ab14c74
kanbara-ap <s.kanbara@alphapolis.co.jp>
2022-04-28 17:28:02 +0900

    third commit 未完成

---
+++ SlideshowApp/ThirdViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ThirdViewController.swift
+//  SlideshowApp
+//
+//  Created by WEBSYSTEM-MAC31 on 2022/04/28.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController,UICollectionViewDataSource,
+UICollectionViewDelegate  {
+    
+    let photos = ["photo1", "photo2","photo3","photo4"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        // "Cell" はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "testcell",
+                                               for: indexPath)
+        
+        // Tag番号を使ってImageViewのインスタンス生成
+        let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
+        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
+        let cellImage = UIImage(named: photos[indexPath.row])
+        // UIImageをUIImageViewのimageとして設定
+        imageView.image = cellImage
+        
+        return testCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
+        return photos.count
+    }
+}
