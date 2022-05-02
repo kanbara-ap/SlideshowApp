b6f0f9be6b23bae527cba53c8dfbcde12513e3ba
kanbara-ap <s.kanbara@alphapolis.co.jp>
2022-05-02 09:40:10 +0900

    提出版

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -15,19 +15,32 @@ class ViewController: UIViewController {
     var timecount : Int = 0
     var timer :Timer!
     
-    var image1,image2,image3,image4 : UIImage!
-    var imageset : [UIImage] = []
+    var imageset : [UIImage]  = []
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        if let image1 = UIImage(named: "photo1"),
+           let image2 = UIImage(named: "photo2"),
+           let image3 = UIImage(named: "photo3"),
+           let image4 = UIImage(named: "photo4"){
+            imageset = [image1,image2,image3,image4]
+        }
+        
+        
+        
+    }
+    
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         //画像の読み込みと一括管理するための配列作成
-        self.image1 = UIImage(named:"photo1")
-        self.image2 = UIImage(named:"photo2")
-        self.image3 = UIImage(named:"photo3")
-        self.image4 = UIImage(named:"photo4")
-        self.imageset = [image1,image2,image3,image4]
+
         
         imageview.image = imageset[timecount]
         
@@ -63,13 +76,17 @@ class ViewController: UIViewController {
     
     @IBAction func autoimage(_ sender: Any) {
         if timer == nil {
+            nextbottun.isEnabled = false
             //２秒ごとにupdateTimerを動かす
             self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
         } else{
             self.timer.invalidate()
             self.timer = nil
+            nextbottun.isEnabled = true
         }
     }
+    @IBOutlet weak var nextbottun: UIButton!
+    
     
     //SecondViewから画面を戻す用の関数
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
@@ -79,13 +96,13 @@ class ViewController: UIViewController {
     
     //画像表示用の関数
     func DisplayImage(_ count : inout Int){
-        if (count <= 3 && count >= 0){
+        if (count <= imageset.count - 1 && count >= 0){
             imageview.image = imageset[count]
-        }else if count >= 4{
+        }else if count >= imageset.count - 1 {
             count = 0
             imageview.image = imageset[count]
         }else{
-            count = 3
+            count = imageset.count - 1
             imageview.image = imageset[count]
         }
     
